590435fb31ba5452c3461ea77c33fd627b7fe1ee
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2021-08-30 20:07:29 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+console.log("hello")
+const translate_btn=document.querySelector("#translator");
+let input_text=document.querySelector("#userInput");
+let output=document.querySelector("#output");
+
+const serverURL = "https://api.funtranslations.com/translate/morse.json";
+
+function createURL(){
+    return serverURL +"?"+"text=" +input_text.value
+}
+function errorHandler(error) {
+    console.log("error occured", error);
+    output.innerText="Something is wrong with server! Please try after some time.";
+    setTimeout(()=>{
+        input_text.value='';
+        output.innerText='';
+    },2500)
+    
+}
+translate_btn.addEventListener("click", ()=>{
+    if(input_text.value!=''){
+        fetch(createURL(input_text.value))
+        .then(response =>response.json())
+        .then(json=>{
+            console.log(json)
+            output.innerHTML = json.contents.translated;
+        })
+        .catch(errorHandler)
+
+    }else{
+        output.innerText = "Please enter some data to process it";
+        setTimeout(()=>{
+            output.innerText=''
+        },2500)
+    }
+        
+})
+
+
